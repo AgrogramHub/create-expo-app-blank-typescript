06cc88ec6ebd5e416df6d7b296fa58825051587f
mces58 <ceser446@gmail.com>
2024-08-22 15:23:43 +0300

    fix(ui): color 3

--- check-tense-commit.ts
+++ check-tense-commit.ts
@@ -6,7 +6,7 @@ const commitMessage = fs.readFileSync(commitMessageFile, 'utf8').trim();
 console.log(commitMessage);
 
 const doc = nlp(commitMessage);
-const verbs = doc.verbs().terms().out('array'); // Verbs'i array olarak alıyoruz
+const verbs = doc.verbs().terms().out('array');
 console.log('Detected Verbs: ', verbs);
 
 if (verbs.length === 0) {
@@ -19,6 +19,12 @@ const isPastTense = verbs.some((verb: string) => {
   console.log(`Verb: ${verb}, Past Tense: ${pastTense}`);
   return pastTense === verb;
 });
+
+const isVerb = verbs.some((verb: string) => nlp(verb).verbs().out('array').length > 0);
+if (!isVerb) {
+  console.log('Commit message must contain at least one verb');
+  process.exit(1);
+}
 
 if (isPastTense) {
   console.log('Commit message is in past tense');
