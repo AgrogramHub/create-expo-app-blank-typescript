6d8564a5cf4b06d11ed0636c0f538eef01fd8c58
mces58 <ceser446@gmail.com>
2024-08-22 17:52:55 +0300

    implements

--- check-tense-commit.ts
+++ check-tense-commit.ts
@@ -7,7 +7,7 @@ if (!commitMessageFile) {
   process.exit(1);
 }
 
-let commitMessage: string;
+let commitMessage: string; // Read the commit message from the file
 
 try {
   commitMessage = fs.readFileSync(commitMessageFile, 'utf8').trim();
@@ -24,14 +24,20 @@ if (verbs.length === 0) {
   process.exit(1);
 }
 
-const isPastTense = verbs.some((verb: string) => {
-  const pastTense = nlp(verb).verbs().toPastTense().out('text');
-  return pastTense === verb;
-});
+const presentTenseMessage = nlp(commitMessage).verbs().toPresentTense().out('text');
+
+const isPastTense = nlp(presentTenseMessage)
+  .verbs()
+  .some((verb) => {
+    const pastTense = nlp(verb.toString()).verbs().toPastTense().out('text');
+    return pastTense === verb;
+  });
 
 if (isPastTense) {
-  console.log('Commit message is in past tense. Verb:', verbs.join(', '));
+  console.log('Commit message is still in past tense');
   process.exit(1);
 } else {
-  console.log('Success: ' + commitMessage);
+  // Save the corrected message back to the file
+  fs.writeFileSync(commitMessageFile, presentTenseMessage);
+  console.log('Success: ' + presentTenseMessage);
 }
