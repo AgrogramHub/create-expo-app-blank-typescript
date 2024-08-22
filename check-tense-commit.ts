7164416a2ebdc0c5ff359f82c9c76b1429129916
mc-es <mehmetcan.eser.dev@gmail.com>
2024-08-22 17:18:16 +0300

    feat(commit): complate

--- check-tense-commit.ts
+++ check-tense-commit.ts
@@ -2,42 +2,37 @@ import nlp from 'compromise';
 import fs from 'fs';
 
 const commitMessageFile = process.argv[2];
-const commitMessage = fs.readFileSync(commitMessageFile, 'utf8').trim();
-console.log(commitMessage);
-
-const doc = nlp(commitMessage);
-const tokens = doc.terms().out('array'); // Mesajı tokenlara ayırıyoruz
-console.log('Detected Tokens: ', tokens);
-
-if (tokens.length === 0) {
-  console.log('Commit message must contain at least one token');
+if (!commitMessageFile) {
+  console.error('Please provide a commit message file path.');
   process.exit(1);
 }
 
-const isPastTense = tokens.some((token: string) => {
-  const tokenDoc = nlp(token);
-  const tokenVerbs = tokenDoc.verbs().out('array');
-  if (tokenVerbs.length > 0) {
-    const pastTense = tokenDoc.verbs().toPastTense().out('text');
-    console.log(`Token: ${token}, Past Tense: ${pastTense}`);
-    return pastTense === token;
-  }
-  return false;
+let commitMessage: string;
+
+try {
+  commitMessage = fs.readFileSync(commitMessageFile, 'utf8').trim();
+} catch (error: any) {
+  console.error('Error reading the commit message file:', error.message);
+  process.exit(1);
+}
+
+const doc = nlp(commitMessage);
+const verbs = doc.verbs().terms().out('array');
+
+if (verbs.length === 0) {
+  console.log('Commit message must contain at least one verb');
+  process.exit(1);
+}
+
+const isPastTense = verbs.some((verb: string) => {
+  const pastTense = nlp(verb).verbs().toPastTense().out('text');
+  console.log(`Verb: ${verb}, Past Tense: ${pastTense}`);
+  return pastTense === verb;
 });
 
 if (isPastTense) {
   console.log('Commit message is in past tense');
   process.exit(1);
-}
-
-const isVerb = tokens.some((token: string) => {
-  const tokenDoc = nlp(token);
-  return tokenDoc.verbs().out('array').length > 0;
-});
-
-if (!isVerb) {
-  console.log('Commit message must contain at least one verb');
-  process.exit(1);
 } else {
-  console.log('Commit message is in present tense and contains at least one verb');
+  console.log('Success: ' + commitMessage);
 }
