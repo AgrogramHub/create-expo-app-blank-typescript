74844e8caed7be576f86a34c784320f9d6bdf132
mc-es <mehmetcan.eser.dev@gmail.com>
2024-08-22 22:32:58 +0300

    feat(commit-lint): add commit-lint workflow

--- commitlint.config.js
+++ commitlint.config.js
@@ -10,7 +10,7 @@ module.exports = {
     'type-case': [2, 'always', 'lower-case'],
     'scope-case': [2, 'always', 'kebab-case'],
     'subject-empty': [2, 'never'],
-    'subject-case': [2, 'always', ['lower-case']],
+    'subject-case': [2, 'always', ['lower-case', 'sentence-case']],
     'header-max-length': [2, 'always', 100],
     'body-leading-blank': [0, 'always'],
     'subject-full-stop': [2, 'never', '.'],
