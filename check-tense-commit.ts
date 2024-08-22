09df64dfd9b3e16a2cb9842800b2795cfac2cd9a
mces58 <ceser446@gmail.com>
2024-08-22 14:50:41 +0300

    fix(ui): implemented

--- check-tense-commit.ts
+++ check-tense-commit.ts
@@ -1,26 +1,26 @@
-const fs = require('fs');
-const nlp = require('compromise');
-const commitMsgFile = process.argv[2];
+import nlp from 'compromise';
 
-// Commit mesajını oku
-const commitMsg = fs.readFileSync(commitMsgFile, 'utf8').trim();
-console.log(commitMsg);
+const commitMessage = process.argv[2];
 
-// NLP ile mesajı analiz et
-const doc = nlp(commitMsg);
-const verbs = doc.verbs().data();
+const doc = nlp(commitMessage);
+const verbs = doc.verbs().out('array');
 console.log(verbs);
 
-// Eğer herhangi bir fiil geçmiş zamanda ise hata döndür
 let isPastTense = false;
-for (const verb of verbs) {
-  if (verb.PastTense) {
+
+verbs.forEach((verb: string) => {
+  const tense = nlp(verb).verbs().conjugate()[0];
+  console.log(tense);
+
+  if (
+    (tense as { PastTense?: string }).PastTense &&
+    (tense as { PastTense?: string }).PastTense === verb
+  ) {
     isPastTense = true;
-    break;
   }
-}
+});
 
 if (isPastTense) {
-  console.error('Commit messages should be in present tense.');
+  console.log('Commit message is in past tense');
   process.exit(1);
 }
